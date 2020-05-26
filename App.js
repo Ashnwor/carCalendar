e090c3fa7562bed0ec6b0d7bcb6d00b02bfc9349
İlyas <ashnwor@gmail.com>
2020-05-27 01:34:38 +0300

    general: add list page, FAB

--- App.js
+++ App.js
@@ -5,28 +5,65 @@ import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Provider as PaperProvider } from 'react-native-paper';
-import { TextInput, Button } from 'react-native-paper';
+import { TextInput, Button, FAB, List as ListItem } from 'react-native-paper';
+
+function OpenList(navigation, day) {
+  console.log(day);
+  navigation.navigate('List');
+}
 
 function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Calendar style={styles.calendar} onDayPress={(day) => { console.log('selected day', day); }} minDate={Date()}
+      <Calendar style={styles.calendar} onDayPress={(day) => { OpenList(navigation, day); }} minDate={Date()}
       />
+    </View>
+  )
+}
 
-      <Button mode='contained' onPress={() => navigation.navigate('Details')} style={{width: '30%', alignSelf: 'center'}} >TEST</Button>
+function List({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <ListItem.Item
+        title="First Item"
+        description="Item description"
+        left={props => <ListItem.Icon {...props} icon="folder" />}
+      />
+      <ListItem.Item
+        title="First Item"
+        description="Item description"
+        left={props => <ListItem.Icon {...props} icon="folder" />}
+      />
+      <ListItem.Item
+        title="First Item"
+        description="Item description"
+        left={props => <ListItem.Icon {...props} icon="folder" />}
+      />
+      <ListItem.Item
+        title="First Item"
+        description="Item description"
+        left={props => <ListItem.Icon {...props} icon="folder" />}
+      />
+      <ListItem.Item
+        title="First Item"
+        description="Item description"
+        left={props => <ListItem.Icon {...props} icon="folder" />}
+      />
+      <FAB
+        style={styles.fab}
+        icon="plus"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   )
 }
 
 function Details({ navigation }) {
-  const [value, onChangeText] = React.useState('Placeholder');
-
   return (
     <View style={styles.container}>
-      <TextInput label='Plaka'/>
-      <TextInput label='Plaka'/>
-      <TextInput label='Plaka'/>
-      <TextInput label='Plaka'/>
+      <TextInput label='Plaka' />
+      <TextInput label='Marka' />
+      <TextInput label='Model' />
       <Button mode='contained' onPress={() => navigation.navigate('Details')} style={styles.saveBtn}>KAYDET</Button>
     </View>
   )
@@ -40,6 +77,7 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="Calendar" component={HomeScreen} />
+          <Stack.Screen name="List" component={List} />
           <Stack.Screen name="Details" component={Details} />
         </Stack.Navigator>
       </NavigationContainer>
@@ -64,5 +102,13 @@ const styles = StyleSheet.create({
     width: '30%',
     alignSelf: 'center',
     marginTop: 50
-  }
+  },
+
+  fab: {
+    position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 0,
+  },
+
 });
