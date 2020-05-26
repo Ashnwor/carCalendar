28cead18e7b4825a9279cbe3b47f9c2cde744e53
İlyas <ashnwor@gmail.com>
2020-05-27 00:59:52 +0300

    general: better Buttons, TextInputs

--- App.js
+++ App.js
@@ -1,26 +1,33 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { TextInput, Button } from 'react-native-paper';
 
 function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Calendar style={styles.calendar} onDayPress={(day) => { console.log('selected day', day) }}
+      <Calendar style={styles.calendar} onDayPress={(day) => { console.log('selected day', day); }} minDate={Date()}
       />
 
-      <Button title="TESTING" onPress={() => navigation.navigate('Details')} />
+      <Button mode='contained' onPress={() => navigation.navigate('Details')} style={{width: '30%', alignSelf: 'center'}} >TEST</Button>
     </View>
   )
 }
 
 function Details({ navigation }) {
+  const [value, onChangeText] = React.useState('Placeholder');
+
   return (
     <View style={styles.container}>
-
-      <Text>Calendar Development</Text>
+      <TextInput label='Plaka'/>
+      <TextInput label='Plaka'/>
+      <TextInput label='Plaka'/>
+      <TextInput label='Plaka'/>
+      <Button mode='contained' onPress={() => navigation.navigate('Details')} style={styles.saveBtn}>KAYDET</Button>
     </View>
   )
 }
@@ -29,12 +36,15 @@ const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={Details} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Calendar" component={HomeScreen} />
+          <Stack.Screen name="Details" component={Details} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+
   );
 }
 
@@ -42,13 +52,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 40,
+    paddingVertical: 10,
   },
 
   calendar: {
     width: '100%',
     height: '80%',
   },
+
+  saveBtn: {
+    width: '30%',
+    alignSelf: 'center',
+    marginTop: 50
+  }
 });
