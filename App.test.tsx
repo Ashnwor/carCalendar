157f42a0b7ec71919708b5a1d9bf1601ec1bd296
İlyas <ashnwor@gmail.com>
2020-07-19 15:28:26 +0300

    misc: simple <App /> test with jest

---
+++ App.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, act } from 'react-native-testing-library';
+
+import App from './App';
+
+describe('<App />', () => {
+	it('has 1 child', async () => {
+		const tree = render(<App />).toJSON();
+		await act(async () => expect(tree.children.length).toBe(1));
+	});
+});
